7b8d5cf76b0614758f576f1b22cf399174572faf
Moksha Supreeth <bmms201@gmail.com>
2026-06-07 06:59:00 +0530

    remove sms debug logs

--- services/sms.js
+++ services/sms.js
@@ -1,12 +1,3 @@
-console.log(
-'FAST2SMS EXISTS:',
-!!process.env.FAST2SMS_API_KEY
-);
-
-console.log(
-'FAST2SMS LENGTH:',
-process.env.FAST2SMS_API_KEY?.length
-);
 const axios = require('axios');
 
 const FAST2SMS_KEY =
