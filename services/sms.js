03507a89ceed054c2641fd05cbdd12dfff548ab4
Moksha Supreeth <bmms201@gmail.com>
2026-06-07 06:28:47 +0530

    fix fast2sms

--- services/sms.js
+++ services/sms.js
@@ -1,22 +1,53 @@
 const axios = require('axios');
 
-const FAST2SMS_KEY = 'uxqChZ7t6pzne1WELBUyRKJjO9VFgDGo2sHvSfkMcbiAmrI5a4uUNz0W4yMHQTF1CZdt2qfrBsLmipoa';
+const FAST2SMS_KEY =
+  process.env.FAST2SMS_API_KEY;
 
-exports.sendSMSOTP = async (phone, otp) => {
-  // Remove +91 or 91 prefix, keep only 10 digits
-  const cleanPhone = phone.replace(/^\+91|^91/, '').replace(/\D/g, '');
-  
-  const response = await axios.post('https://www.fast2sms.com/dev/bulkV2', {
-    route: 'otp',
-    variables_values: otp,
-    numbers: cleanPhone,
-  }, {
-    headers: {
-      authorization: FAST2SMS_KEY,
-      'Content-Type': 'application/json',
-    }
-  });
+exports.sendSMSOTP =
+async (phone, otp) => {
 
-  console.log('SMS Response:', response.data);
-  return response.data;
+  const cleanPhone =
+    phone
+      .replace(/^\+91|^91/, '')
+      .replace(/\D/g, '');
+
+  try {
+
+    const response =
+      await axios.post(
+        'https://www.fast2sms.com/dev/bulkV2',
+        {
+          route: 'q',
+
+          message:
+            `Your OTP is ${otp}`,
+
+          numbers:
+            cleanPhone
+        },
+        {
+          headers: {
+            authorization:
+              FAST2SMS_KEY
+          }
+        }
+      );
+
+    console.log(
+      'SMS Response:',
+      response.data
+    );
+
+    return response.data;
+
+  } catch (err) {
+
+    console.log(
+      'SMS ERROR:',
+      err.response?.data ||
+      err.message
+    );
+
+    throw err;
+  }
 };
